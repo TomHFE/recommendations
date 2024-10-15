3be4563163d2b93d85d31341ad931c52b5fd5062
Marion <125993993+Marion34-dev@users.noreply.github.com>
2024-10-15 15:55:16 +0100

    Update recipe.js

--- api/models/recipe.js
+++ api/models/recipe.js
@@ -1,55 +1,93 @@
 const mongoose = require("mongoose");
+
+// A Schema defines the "shape" of entries in a collection. This is similar to
+// defining the columns of an SQL Database.
 const RecipeSchema = new mongoose.Schema({
-  user_id: {
+  created_at: { type: Date, default: Date.now },
+  user: {
     type: mongoose.SchemaTypes.ObjectId,
-    ref: 'User',
+    ref: "User",
     required: true,
   },
+  comments: [
+    {
+      comment: String,
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  favourites: [
+    {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+        required: true,
+    },
+  ],
+//   user_id: {
+//     type: mongoose.SchemaTypes.ObjectId,
+//     ref: "User",
+//     required: true,
+//   },
   title: String,
   image: String,
   summary: String,
   instructions: String,
-  created_at: { type: Date, default: Date.now },
-  SearchingParameters: [{
-      nationalities: {
+  SearchingParameters: [
+    {
+      nationalities: [{
+        type: String,
+        required: false,
+      }],
+      dishType: [
+        {
           type: String,
           required: false,
         },
-        dishType: [{
-            type: String,
-            required: false
-        }],
-        preparationMinutes: Number,
-        cookingMinutes: Number,
-        servings: Number,
-        ingredients: [{
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: 'Ingredients',
-            required: true,
-        }],
-        Requirements: [{
-           allergies: [{
-            type: String,
-            required: false,
-           }],
-           vegeterian: Boolean,
-           vegan: Boolean,
-           glutenFree: Boolean,
-           dairyFree: Boolean,
-           healthy: Boolean,
-           costFriendly: Number,
-           readyInMinutes: Number, required: false,
-        }]
-  }],
-  comments: [{
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'Comment',
-  }],
-  favourites: [{
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'User',
-    required: false,
-  }],
+      ],
+      preparationMinutes: Number,
+      cookingMinutes: Number,
+      servings: Number,
+      Requirements: [
+        {
+          allergies: [
+            {
+              nuts: Boolean,
+              shellfish: Boolean,
+              dairy: Boolean,
+              soy: Boolean,
+              eggs: Boolean,
+              required: false,
+            },
+          ],
+          vegeterian: Boolean,
+          vegan: Boolean,
+          pescatarian: Boolean,
+          glutenFree: Boolean,
+          dairyFree: Boolean,
+          healthy: Boolean,
+          costFriendly: Number,
+          readyInMinutes: Number,
+          required: false,
+        },
+      ],
+    },
+  ],
+  ingredients: [
+  {
+    name: String,
+    quantity: Number,
+    unit: String,
+}]
+
 });
+
+// We use the Schema to create the Post model. Models are classes which we can
+// use to construct entries in our Database.
 const Recipe = mongoose.model("Recipe", RecipeSchema);
+
+// These lines will create a test post every time the server starts.
+// You can delete this once you are creating your own posts.
+//const dateTimeString = new Date().toLocaleString("en-GB");
+//new Post({ message: `Test message, created at ${dateTimeString}` }).save();
+
 module.exports = Recipe;
