97f2bdd5b48ec1a95570928675b80cc50a74999c
Avnita Bhandal <97522740+NeetKB@users.noreply.github.com>
2024-10-21 11:13:09 +0100

    fixed comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,14 @@ import { LoginPage } from "./pages/Login/LoginPage";
 import { SignupPage } from "./pages/Signup/SignupPage";
 import { FeedPage } from "./pages/Feed/FeedPage";
 import { Profile } from "./pages/Profile/profile";
-import RecipePage from './components/recipePage'
+import RecipePage from "./components/recipePage";
 import UsersFollowingPage from "./components/follow_component/handleFollowing";
 import UsersFollowerPage from "./components/follower_component/handleFollowing";
 import { MainLayout } from "./layouts/MainLayout";
 //import { PublicProfile } from "./pages/PublicProfile/PublicProfile";
 
 // docs: https://reactrouter.com/en/main/start/overview
-// 
+//
 const router = createBrowserRouter([
   {
     path: "/login",
@@ -25,30 +25,26 @@ const router = createBrowserRouter([
   },
   { path: "/home", element: <HomePage /> },
   {
-    
-    
-      path: "/", // Main layout for all these routes
-      element: <MainLayout />, // Wrap child routes with MainLayout
-      children: [
-        { path: "/recipes", element: <FeedPage /> },
-        { path: "/profile", element: <Profile /> },
-        { path: "/recipe_page", element: <RecipePage /> },
-        { path: "/user_followers", element: <UsersFollowerPage /> },
-        { path: "/user_following", element: <UsersFollowingPage /> },
-      ],
-    
+    path: "/", // Main layout for all these routes
+    element: <MainLayout />, // Wrap child routes with MainLayout
+    children: [
+      { path: "/recipes", element: <FeedPage /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/recipe_page", element: <RecipePage /> },
+      { path: "/user_followers", element: <UsersFollowerPage /> },
+      { path: "/user_following", element: <UsersFollowingPage /> },
+    ],
   },
+]);
 //   {
 //     path: "/recipes/filtered",
 //     element: <FeedPage />,
 //   },
 
-
-
 function App() {
   return (
     <>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
   );
 }
