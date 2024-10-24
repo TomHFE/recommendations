1f1ba5e5c4bca3009d99a702176afb21b99fcf1b
Marion <marionjung@myyahoo.com>
2024-10-24 10:50:07 +0100

    'Favourited' & Navbar

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -2,13 +2,14 @@ import { Outlet } from 'react-router-dom';
 import NavBar from '../navbar/navbar';
 
 export const MainLayout = () => {
+    const token = localStorage.getItem("token"); //  check authentication
+
     return (
-        <div style={{margin: "3rem"}}>
-            <NavBar/>
+        <div style={{ margin: "3rem" }}>
+            {token && <NavBar />} 
             <main>
-            <Outlet />
+                <Outlet />
             </main>
-            
         </div>
-    )
+    );
 }
